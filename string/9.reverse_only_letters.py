b73dfd7c8f915f11152f8dd203ccf4cfdecedc8c
vishalsharma14 <vishal.sharma9597@gmail.com>
2019-04-02 11:59:24 -0400

    String 9.reverse_only_letters.py

--- string/9.reverse_only_letters.py
+++ string/9.reverse_only_letters.py
@@ -1,7 +1,8 @@
 """
     LeetCode 917. Reverse Only Letter
 
-    Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+    Given a string S, return the "reversed" string where all characters that are
+    not a letter stay in the same place, and all letters reverse their positions.
 
 
 
@@ -29,9 +30,21 @@
 
 def reverse_only_letters(S):
     """
-
-        :param S:
-        :return:
+        :param S: Input String
+        :return: Updated reversed string
     """
+    lower = 0
+    high = len(S) - 1
+    char_list = list(S)
+    while lower < high:
+        if not (97 <= ord(char_list[lower]) <= 122 or 65 <= ord(char_list[lower]) <= 90):
+            lower += 1
+        elif not (97 <= ord(char_list[high]) <= 122 or 65 <= ord(char_list[high]) <= 90):
+            high -= 1
+        else:
+            char_list[lower], char_list[high] = char_list[high], char_list[lower]
+            lower += 1
+            high -= 1
+    return "".join(char_list)
 
 
