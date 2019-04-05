279d763f019c2d3a49fc5b6eccab6b1038904eba
vishalsharma14 <vishal.sharma9597@gmail.com>
2019-04-05 11:17:44 -0400

    String 22.no_of_segments_in_a_string.py

---
+++ string/22.no_of_segments_in_a_string.py
@@ -0,0 +1,23 @@
+"""
+    LeetCode 434. Number of Segments in a String
+
+    Count the number of segments in a string, where a segment is defined to be a
+    contiguous sequence of non-space characters.
+
+    Please note that the string does not contain any non-printable characters.
+
+    Example:
+
+    Input: "Hello, my name is John"
+    Output: 5
+"""
+
+def no_of_segments_in_a_string(input_string):
+    """
+        :param str: Input string
+        :return: Number of segments separated by a space
+    """
+    input_string = input_string.strip()
+    if len(input_string) == 0:
+        return 0
+    return len(input_string.split())
